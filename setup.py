7cf2f8de8cb835e474a3622dd670a0c557a63140
SinisterRj <81830673+SinisterRj@users.noreply.github.com>
2021-08-30 16:18:43 -0300

    Fix repo URL

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Rodrigo C. C. Silva"
 plugin_author_email = "rcesarcs@hotmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/SinisterRj/SafetyPrinter"
+plugin_url = "https://github.com/SinisterRj/Octoprint_SafetyPrinter"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
