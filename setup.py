3ca017cd94087b02307fae75af16b555ec62606a
SinisterRj <rcesarcs@hotmail.com>
2021-09-10 12:15:22 -0300

    plugin_requires fixed

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/SinisterRj/Octoprint_SafetyPrinter"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["Safety Printer MCU - https://github.com/SinisterRj/SafetyPrinter/wiki"]
+plugin_requires = []
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
